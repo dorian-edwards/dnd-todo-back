0e1a835e1573255ac21336e5068a94b81194a273
Dorian Edwards <de.pharmd2018@gmail.com>
2022-08-12 01:44:13 -0400

    install cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const dbConnect = require('./database')
 const Task = require('./models/task')
 const app = express()
@@ -10,6 +11,7 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
